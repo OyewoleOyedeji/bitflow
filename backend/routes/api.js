7870a221e924bff0893361a8009bd869dbda8165
OyewoleOyedeji <57059138+LokotamaTheMastermind@users.noreply.github.com>
2022-01-07 15:23:08 +0100

    Code refactorings
    Updated the routes with better code refactorings, all the routes now
    follow the DRY (don't repeat yourself) principle

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -20,114 +20,89 @@ if (process.env.NODE_ENV !== "production") {
 // Generate private ID
 const privateId = hash(publicId);
 
-// GET /api/ route
-router.get("/", function (req, res, next) {
-  res.statusCode = 405;
-  res.statusMessage = "Sorry /api/ route doesn't support GET requests";
-  res.json({
-    code: res.statusCode,
-    message: res.statusMessage,
-    supportedMethod: ["POST"],
-  });
-});
+let outputJSON = {}
 
-router.put("/", function (req, res, next) {
+router.route("/").get(function (req, res) {
   res.statusCode = 405;
-  res.statusMessage = "Sorry /api/ route doesn't support PUT requests";
-  res.json({
-    code: res.statusCode,
-    message: res.statusMessage,
-    supportedMethod: ["POST"],
-  });
-});
-
-router.patch("/", function (req, res, next) {
+  outputJSON.code = res.statusCode;
+  outputJSON.reason = "Sorry /api/ route doesn't support " + req.method.toUpperCase() + " requests";
+  outputJSON.supportedMethods = ["POST"];
+  res.json(outputJSON);
+}).put(function (req, res) {
   res.statusCode = 405;
-  res.statusMessage = "Sorry /api/ route doesn't support PATCH requests";
-  res.json({
-    code: res.statusCode,
-    message: res.statusMessage,
-    supportedMethod: ["POST"],
-  });
-})
-
-router.delete("/", function (req, res, next) {
+  outputJSON.code = res.statusCode;
+  outputJSON.reason = "Sorry /api/ route doesn't support " + req.method.toUpperCase() + " requests";
+  outputJSON.supportedMethods = ["POST"];
+  res.json(outputJSON);
+}).patch(function (req, res) {
   res.statusCode = 405;
-  res.statusMessage = "Sorry /api/ route doesn't support DELETE requests";
-  res.json({
-    code: res.statusCode,
-    message: res.statusMessage,
-    supportedMethod: ["POST"],
-  });
-})
-
-// POST /api/ route
-router.post("/", function (req, res, next) {
+  outputJSON.code = res.statusCode;
+  outputJSON.reason = "Sorry /api/ route doesn't support " + req.method.toUpperCase() + " requests";
+  outputJSON.supportedMethods = ["POST"];
+  res.json(outputJSON);
+}).delete(function (req, res) {
+  res.statusCode = 405;
+  outputJSON.code = res.statusCode;
+  outputJSON.reason = "Sorry /api/ route doesn't support " + req.method.toUpperCase() + " requests";
+  outputJSON.supportedMethods = ["POST"];
+  res.json(outputJSON);
+}).post(function (req, res) {
+  // Get X-PUBLIC-ID header (if any)
   const publicIdHeader = req.headers["x-public-id"];
+
+  // Reset global outputJSON variable
+  outputJSON = {}
 
   console.log(req.body);
 
-  if (publicIdHeader !== "") {
-    if (publicIdHeader === undefined) {
-      res.statusCode = 401;
-      res.statusMessage = "Couldn't find the X-PUBLIC-ID header";
-      res.json({
-        code: res.statusCode,
-        message: res.statusMessage,
-        suggestion: "Include the X-PUBLIC-ID header for the next request",
-      });
-    } else {
-      const publicDigest = hash(publicIdHeader);
-
-      if (publicDigest === privateId) {
-        // Create nodemailer transporter with below config
-        let transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            user: process.env.GMAIL_ID,
-            pass: process.env.GMAIl_PASSWORD,
-          },
-        });
-
-        // Options to be passed to the email
-        let mailOptions = {
-          from: process.env.GMAIL_ID,
-          to: process.env.GMAIl_ID,
-          subject: "New message from Bitflow",
-          text: "Test email on " + new Date(),
-        };
-
-        // Send the mail and await error or response
-        transporter.sendMail(mailOptions, function (err, data) {
-          if (err) {
-            res.json({
-              error: err,
-            });
-          } else {
-            res.status(200).json({
-              response: data.response,
-              messageId: data.messageId,
-            });
-          }
-        });
-      } else {
-        res.statusCode = 401;
-        res.statusMessage = "Authorization header has been tempered with";
-        res.json({
-          code: res.statusCode,
-          message: res.statusMessage,
-          suggestion: "Sorry there is no suggestion for this issue",
-        });
-      }
-    }
-  } else {
+  // Check if the X-PUBLIC-ID is valid in request
+  if (publicIdHeader === undefined) {
     res.statusCode = 401;
-    res.statusMessage = "Not authorized to peform this action";
-    res.json({
-      code: res.statusCode,
-      message: res.statusMessage,
-      suggestion: "Try checking the X-PUBLIC-ID header is not empty",
-    });
+    outputJSON.code = res.statusCode;
+    outputJSON.reason = "Couldn't find the X-PUBLIC-ID header";
+    res.json(outputJSON);
+  } else {
+    const publicDigest = hash(publicIdHeader);
+
+    if (publicDigest === privateId) {
+      // Create nodemailer transporter with config from the environment
+      let transporter = nodemailer.createTransport({
+        service: "gmail",
+        auth: {
+          user: process.env.GMAIL_ID,
+          pass: process.env.GMAIl_PASSWORD,
+        },
+      });
+
+      // Options to be passed to the email
+      let mailOptions = {
+        from: process.env.GMAIL_ID,
+        to: process.env.GMAIl_ID,
+        subject: "New message from Bitflow",
+        text: "New email from Bitflow on " + new Date(),
+      };
+
+      // Send the mail and await error or response
+      transporter.sendMail(mailOptions, function (err, data) {
+        if (err) {
+          res.status(500)
+          outputJSON.code = res.statusCode;
+          outputJSON.reason = err;
+          res.json(outputJSON);
+        } else {
+          res.status(200).json({
+            response: data.response,
+            messageId: data.messageId,
+          });
+        }
+      });
+
+    } else {
+      res.statusCode = 401;
+      outputJSON.code = res.statusCode;
+      outputJSON.reason = "Unauthorized request";
+      res.json(outputJSON);
+    }
   }
 });
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,25 +1,52 @@
 const express = require("express");
 const router = express.Router();
-require('dotenv').config()
+require("dotenv").config();
 
-/* GET home page. */
-router.get("/", function (req, res, next) {
-  if (req.headers['x-public-id']) {
-    res.json({
-      madeFor: 'Bitflow',
-      shortDescription: "Email sending API",
-      description: "Just another API for sending emails to your inbox, which was hosted on " + process.env.HOST,
-      codeRepository: 'https://github.com/OyewoleOyedeji/bitflow.git',
-      suggestion: 'No need to include the X-PUBLIC-ID header for this route'
-    });
-  } else {
-    res.json({
-      madeFor: 'Bitflow',
-      shortDescription: "Email sending API",
-      description: "Just another API for sending emails to your inbox, which was hosted on " + process.env.HOST,
-      codeRepository: 'https://github.com/OyewoleOyedeji/bitflow.git'
-    });
-  }
-});
+let outputJSON = {};
+
+/* Base route chain */
+router.route("/")
+  .get(function (req, res, next) {
+    outputJSON.madeFor = "Bitflow";
+    outputJSON.shortDescription = "Email sending API";
+    outputJSON.description = "Just another API for sending emails to your inbox, which was hosted on " + process.env.HOST;
+    outputJSON.codeRepository = "https://github.com/OyewoleOyedeji/bitflow.git";
+
+    if (req.headers["x-public-id"]) {
+      outputJSON.suggestion =
+        "No need to include the X-PUBLIC-ID header for this route";
+      res.json(outputJSON);
+    } else {
+      res.json(outputJSON);
+    }
+  })
+  .post(function (req, res, next) {
+    res.statusCode = 405;
+    outputJSON.code = res.statusCode;
+    outputJSON.reason = "Sorry base route doesn't support POST requests";
+    outputJSON.supportedMethods = ["POST"];
+    res.json(outputJSON);
+  })
+  .put(function (req, res, next) {
+    res.statusCode = 405;
+    outputJSON.code = res.statusCode;
+    outputJSON.reason = "Sorry base route doesn't support " + req.method.toUpperCase() + " requests";
+    outputJSON.supportedMethods = ["POST"];
+    res.json(outputJSON);
+  })
+  .patch(function (req, res, next) {
+    res.statusCode = 405;
+    outputJSON.code = res.statusCode;
+    outputJSON.reason = "Sorry base route doesn't support " + req.method.toUpperCase() + " requests";
+    outputJSON.supportedMethods = ["POST"];
+    res.json(outputJSON);
+  })
+  .delete(function (req, res, next) {
+    res.statusCode = 405;
+    outputJSON.code = res.statusCode;
+    outputJSON.reason = "Sorry base route doesn't support " + req.method.toUpperCase() + " requests";
+    outputJSON.supportedMethods = ["POST"];
+    res.json(outputJSON);
+  });
 
 module.exports = router;
